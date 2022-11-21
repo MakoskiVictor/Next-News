33b5c4cb8e4a27eb66dd3b984696d9452a25a668
Makoski Víctor <makoski.ed@gmail.com>
2022-11-21 11:34:52 -0300

    styles modified, articles request

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Head>
+      <title>Next News</title>
+      <meta name="description" content="News page" />
+      <link rel="icon" href="/favicon.ico" />
+  </Head>
+  <Component {...pageProps} />
+  </>
+  )
 }
